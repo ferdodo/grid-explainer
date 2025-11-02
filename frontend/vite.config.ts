562b3c60d1660269743501b9d237f2cde2e6ff96
Thomas Riffard <ferdodo@hotmail.fr>
2025-11-02 14:16:56 +0100

    remove aliases

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,16 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import path from "path";
 
 export default defineConfig({
 	plugins: [react()],
-	resolve: {
-		alias: {
-			"@grid-explainer/core": path.resolve(__dirname, "./core/dist"),
-			"@grid-explainer/design-system": path.resolve(
-				__dirname,
-				"./design-system/dist",
-			),
-		},
-	},
 });
